dd565916f2e015c0e0977b36c07767bac3fd6441
touti <toutati@free.fr>
2014-04-14 12:09:14 +0000

    Report de http://core.spip.org/projects/spip/repository/revisions/21311 et 21312 pour ajouter mime_type dans le array de retour

--- ecrire/inc/distant.php
+++ ecrire/inc/distant.php
@@ -480,6 +480,7 @@ function fichier_copie_locale($source){
 #$a['taille'] = intval
 #$a['extension'] = chaine
 #$a['fichier'] = chaine
+#$a['mime_type'] = chaine
 
 // http://doc.spip.org/@recuperer_infos_distantes
 function recuperer_infos_distantes($source, $max = 0, $charger_si_petite_image = true){
@@ -597,7 +598,8 @@ function recuperer_infos_distantes($source, $max = 0, $charger_si_petite_image =
 		if (preg_match(',<title>(.*?)</title>,ims', $page, $regs))
 			$a['titre'] = corriger_caracteres(trim($regs[1]));
 		if (!$a['taille']) $a['taille'] = strlen($page); # a peu pres
-	}
+	}	
+	$a['mime_type']=$mime_type;
 
 	return $a;
 }
