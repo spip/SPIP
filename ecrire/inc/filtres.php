0bee0ac576a041bd2d308ee34874533d98ef33c4
Debondt Didier <p@henix.be>
2015-08-10 00:09:14 +0200

    On ajoute une class "ancre_spip"

--- ecrire/inc/filtres.php
+++ ecrire/inc/filtres.php
@@ -2780,7 +2780,7 @@ function filtre_pagination_dist($total, $nom, $position, $pas, $liste = true, $m
 
 	// n'afficher l'ancre qu'une fois
 	if (!isset($ancres[$ancre]))
-		$bloc_ancre = $ancres[$ancre] = "<a id='".$ancre."'></a>";
+		$bloc_ancre = $ancres[$ancre] = '<a id="'.$ancre.'" class="ancre_spip"></a>';
 	else $bloc_ancre = '';
 	// liste = false : on ne veut que l'ancre
 	if (!$liste)
