b90d031e591ac15932a0ed7c0954c03f2bb8a8bd
Matthieu Marcillaud <marcimat@rezo.net>
2021-12-15 09:33:19 +0100

    PHP 8.1 #4968 : Attraper les exceptions mysqli. Il faudrait aller plus loin en forçant pour PHP < 8.1 les execptions de mysqli via mysqli_report(), et les utiliser à la place de spip_mysql_errno()

--- ecrire/req/mysql.php
+++ ecrire/req/mysql.php
@@ -241,8 +241,14 @@ function spip_mysql_query($query, $serveur = '', $requeter = true) {
 		}
 		$debug = ' /* ' . mysqli_real_escape_string($link, str_replace('*/', '@/', $debug)) . ' */';
 	}
-
-	$r = mysqli_query($link, $query . $debug);
+	try {
+		$r = mysqli_query($link, $query . $debug);
+	} catch (\mysqli_sql_exception $e) {
+		spip_log('mysqli_sql_exception: ' . $e->getMessage(), 'mysql.' . _LOG_DEBUG);
+		$r = false;
+		// Todo: utiliser l’exception ensuite plutôt que les appels à spip_mysql_errno()
+		// mais il faut pour php < 8.1 forcer les exeptions via mysqli_report().
+	}
 
 	//Eviter de propager le GoneAway sur les autres requetes d'un même processus PHP
 	if ($e = spip_mysql_errno($serveur)) {  // Log d'un Gone Away
