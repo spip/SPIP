e77892244259c7c1ca9ee1d4087b3ca2fdb9ee65
Matthieu Marcillaud <marcimat@rezo.net>
2022-02-23 14:07:31 +0100

    Ne pas étendre SpipCles depuis Cles. Simplement un dépendre

--- ecrire/chiffrer/Cles.php
+++ ecrire/chiffrer/Cles.php
@@ -33,7 +33,7 @@ class Cles /* implements ContainerInterface */ {
         return $key;
     }
 
-	protected function set(
+	public function set(
 		string $name, 
 		#[\SensitiveParameter]
 		string $key
@@ -41,75 +41,7 @@ class Cles /* implements ContainerInterface */ {
 		$this->keys[$name] = $key;
 	}
 
-	/**
-	 * Fournir une sauvegarde chiffree des cles (a l'aide d'une autre clé, comme le pass d'un auteur)
-	 * 
-	 * @param string $withKey Clé de chiffrage de la sauvegarde
-	 * @return string Contenu de la sauvegarde chiffrée générée
-	 */
-	public function backup(
-		#[\SensitiveParameter]
-		string $withKey
-	): string {
-		if (count($this->keys)) {
-			return Chiffrement::chiffrer($this->toJson(), $withKey);
-		}
-		return '';
-	}
-
-	/**
-	 * Restaurer les cles manquantes depuis une sauvegarde chiffree des cles
-	 * (si la sauvegarde est bien valide)
-	 * 
-	 * @param string $backup Sauvegarde chiffrée (générée par backup())
-	 * @param int $id_auteur
-	 * @param string $pass
-	 * @return void
-	 */
-	public function restore(
-		string $backup, 
-		#[\SensitiveParameter]
-		string $password_clair, 
-		#[\SensitiveParameter]
-		string $password_hash,
-		int $id_auteur
-	): bool {
-		if (empty($backup)) {
-			return false;
-		}
-
-		$sauvegarde = Chiffrement::dechiffrer($backup, $password_clair);
-		$json = json_decode($sauvegarde, true);
-		if (!$json) {
-			return false;
-		}
-
-		// cela semble une sauvegarde valide
-		$cles_potentielles = array_map('base64_decode', $json);
-
-		// il faut faire une double verif sur secret_des_auth
-		// pour s'assurer qu'elle permet bien de decrypter le pass de l'auteur qui fournit la sauvegarde
-		// et par extension tous les passwords
-		if (!empty($cles_potentielles['secret_des_auth'])) {
-			if (!Password::verifier($password_clair, $password_hash, $cles_potentielles['secret_des_auth'])) {
-				spip_log("Restauration de la cle `secret_des_auth` par id_auteur $id_auteur erronnee, on ignore", 'chiffrer' . _LOG_INFO_IMPORTANTE);
-				unset($cles_potentielles['secret_des_auth']);
-			}
-		}
-
-		// on merge les cles pour recuperer les cles manquantes
-		$restauration = false;
-		foreach ($cles_potentielles as $name => $key) {
-			if (!$this->has($name)) {
-				$this->set($name, $key);
-				spip_log("Restauration de la cle $name par id_auteur $id_auteur", 'chiffrer' . _LOG_INFO_IMPORTANTE);
-				$restauration = true;
-			}
-		}
-		return $restauration;
-	}
-
-	protected function toJson(): string {
+	public function toJson(): string {
 		$json = array_map('base64_encode', $this->keys);
 		return \json_encode($json);
 	}
--- ecrire/chiffrer/SpipCles.php
+++ ecrire/chiffrer/SpipCles.php
@@ -12,10 +12,11 @@
 
 namespace Spip\Core\Chiffrer;
 
-final class SpipCles extends Cles {
+final class SpipCles {
 	private static array $instances = [];
 
 	private string $file = _DIR_ETC . "cles.php";
+	private Cles $cles;
 
 	public static function instance(string $file = ''): self {
 		if (empty(self::$instances[$file])) {
@@ -28,19 +29,7 @@ final class SpipCles extends Cles {
 		if ($file) {
 			$this->file = $file;
 		}
-		parent::__construct($this->read());
-	}
-
-	private function getKey(string $name, bool $autoInit): ?string {
-		if ($this->has($name)) {
-			return $this->get($name);
-		}
-		if ($autoInit) {
-			$this->generate($name);
-			$this->save();
-			return $this->get($name);
-		}
-        return null;
+		$this->cles = new Cles($this->read());
 	}
 
 	public function getSecretSite(bool $autoInit = true): ?string {
@@ -51,8 +40,89 @@ final class SpipCles extends Cles {
 	}
 
 	public function save(): bool {
-		return ecrire_fichier_securise($this->file, $this->toJson());
+		return ecrire_fichier_securise($this->file, $this->cles->toJson());
 	}
+
+	/**
+	 * Fournir une sauvegarde chiffree des cles (a l'aide d'une autre clé, comme le pass d'un auteur)
+	 * 
+	 * @param string $withKey Clé de chiffrage de la sauvegarde
+	 * @return string Contenu de la sauvegarde chiffrée générée
+	 */
+	public function backup(
+		#[\SensitiveParameter]
+		string $withKey
+	): string {
+		if (count($this->keys)) {
+			return Chiffrement::chiffrer($this->cles->toJson(), $withKey);
+		}
+		return '';
+	}
+
+	/**
+	 * Restaurer les cles manquantes depuis une sauvegarde chiffree des cles
+	 * (si la sauvegarde est bien valide)
+	 * 
+	 * @param string $backup Sauvegarde chiffrée (générée par backup())
+	 * @param int $id_auteur
+	 * @param string $pass
+	 * @return void
+	 */
+	public function restore(
+		string $backup, 
+		#[\SensitiveParameter]
+		string $password_clair, 
+		#[\SensitiveParameter]
+		string $password_hash,
+		int $id_auteur
+	): bool {
+		if (empty($backup)) {
+			return false;
+		}
+
+		$sauvegarde = Chiffrement::dechiffrer($backup, $password_clair);
+		$json = json_decode($sauvegarde, true);
+		if (!$json) {
+			return false;
+		}
+
+		// cela semble une sauvegarde valide
+		$cles_potentielles = array_map('base64_decode', $json);
+
+		// il faut faire une double verif sur secret_des_auth
+		// pour s'assurer qu'elle permet bien de decrypter le pass de l'auteur qui fournit la sauvegarde
+		// et par extension tous les passwords
+		if (!empty($cles_potentielles['secret_des_auth'])) {
+			if (!Password::verifier($password_clair, $password_hash, $cles_potentielles['secret_des_auth'])) {
+				spip_log("Restauration de la cle `secret_des_auth` par id_auteur $id_auteur erronnee, on ignore", 'chiffrer' . _LOG_INFO_IMPORTANTE);
+				unset($cles_potentielles['secret_des_auth']);
+			}
+		}
+
+		// on merge les cles pour recuperer les cles manquantes
+		$restauration = false;
+		foreach ($cles_potentielles as $name => $key) {
+			if (!$this->cles->has($name)) {
+				$this->cles->set($name, $key);
+				spip_log("Restauration de la cle $name par id_auteur $id_auteur", 'chiffrer' . _LOG_INFO_IMPORTANTE);
+				$restauration = true;
+			}
+		}
+		return $restauration;
+	}
+
+	private function getKey(string $name, bool $autoInit): ?string {
+		if ($this->cles->has($name)) {
+			return $this->cles->get($name);
+		}
+		if ($autoInit) {
+			$this->cles->generate($name);
+			$this->save();
+			return $this->cles->get($name);
+		}
+        return null;
+	}
+
 	private function read(): array {
 		lire_fichier_securise($this->file, $json);
 		if (
@@ -64,5 +134,4 @@ final class SpipCles extends Cles {
 		}
 		return [];
 	}
-
 }
